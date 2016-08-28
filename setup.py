2576a3fca90efba3b0c02deab624ff23e3177f81
Ariel Nuñez <ingenieroariel@gmail.com>
2016-08-28 18:19:24 -0500

    Include migrations in package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "geonode-dialogos",
-    version = "0.6",
+    version = "0.7",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     maintaner = "Geonode Developers",
@@ -15,6 +15,7 @@ setup(
     packages = [
         "dialogos",
         "dialogos.templatetags",
+        "dialogos.migrations",
     ],
     classifiers = [
         "Development Status :: 3 - Alpha",
