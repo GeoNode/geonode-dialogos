1a0dd7ebed3d7bf79d8b8cdf8f2177184dfd1f3e
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-03-02 11:33:33 +0100

    Compatibility with Django 1.9+

--- dialogos/models.py
+++ dialogos/models.py
@@ -1,13 +1,9 @@
-import django
 from datetime import datetime
 
 from django.db import models
 
 from django.conf import settings
-if django.VERSION < (1, 9):
-    from django.contrib.contenttypes.generic import GenericForeignKey
-else:
-    from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "geonode-dialogos",
-    version = "0.8",
+    version = "0.9",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     maintaner = "Geonode Developers",
