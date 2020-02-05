9856147002c0adafafcf1defd2294116ec8defa4
afabiani <alessio.fabiani@geo-solutions.it>
2020-02-05 10:39:57 +0100

    - Python3 / Django 2+ official release 3.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="geonode-dialogos",
-    version="3.0.1b",
+    version="3.0.1",
     author="Eldarion",
     author_email="development@eldarion.com",
     maintaner="Geonode Developers",
